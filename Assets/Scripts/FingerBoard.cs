3c6c17d959cc618187fabe68e7783d4be249337b
Riley Sikes <907riley@gmail.com>
2023-06-05 16:55:31 -0700

    Init commit

---
+++ Assets/Scripts/FingerBoard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerBoard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onGreenDClick()
+    {
+        Debug.Log("Green Clicked");
+    }
+}
