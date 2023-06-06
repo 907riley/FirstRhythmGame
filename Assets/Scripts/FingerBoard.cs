0b15dc92c180bd766cfa80edf6847c0f049c9f60
Riley Sikes <907riley@gmail.com>
2023-06-05 17:30:27 -0700

    have first finger button working

--- Assets/Scripts/FingerBoard.cs
+++ Assets/Scripts/FingerBoard.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FingerBoard : MonoBehaviour
 {
+    [SerializeField] GameObject greenButton;
+    [SerializeField] GameObject redButton;
+    [SerializeField] GameObject yellowButton;
+    [SerializeField] GameObject blueButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +18,60 @@ public class FingerBoard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            onGreenClick();
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            onGreenRelease();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            onRedClick();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            onYellowClick();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            onBlueClick();
+        }
     }
 
-    public void onGreenDClick()
+    public void onGreenClick()
     {
-        Debug.Log("Green Clicked");
+        Color color = greenButton.GetComponent<SpriteRenderer>().color;
+        color[1] -= .3f;
+        greenButton.GetComponent<SpriteRenderer>().color = color;
+        Debug.Log("Green Clicked: " + color);
+    }
+
+    public void onGreenRelease()
+    {
+        Color color = greenButton.GetComponent<SpriteRenderer>().color;
+        color[1] += .3f;
+        greenButton.GetComponent<SpriteRenderer>().color = color;
+        Debug.Log("Green Clicked: " + color);
+    }
+
+    public void onRedClick()
+    {
+        Debug.Log("Red Clicked");
+    }
+
+    public void onYellowClick()
+    {
+        Debug.Log("Yellow Clicked");
+    }
+
+    public void onBlueClick()
+    {
+        Debug.Log("Blue Clicked");
     }
 }
