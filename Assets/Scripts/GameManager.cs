5b0d37a682313cf9ca100e5f1b97d390743e8c40
Riley Sikes <907riley@gmail.com>
2023-06-07 12:20:37 -0700

    current streak UI working

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,26 +11,36 @@ public class GameManager : MonoBehaviour
     public int currentStreak = 0;
     public int missClicks = 0;
 
+    [SerializeField] TextMeshProUGUI streakText;
+
     public void OnMissClick()
     {
         ++missClicks;
         currentStreak = 0;
+        SetStreak();
     }
 
     public void OnMissedNote()
     {
         ++notesMissed;
         currentStreak = 0;
+        SetStreak();
     }
 
     public void OnNoteHit()
     {
         ++notesHit;
         ++currentStreak;
+        SetStreak();
     }
 
     public void OnNoteSpawned()
     {
         ++notesSpawned;
+    }
+
+    private void SetStreak()
+    {
+        streakText.text = currentStreak.ToString();
     }
 }
